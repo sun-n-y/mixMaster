581c505522c79e48e88867bd86adde7931b31d8e
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-22 15:57:02 -0500

    add nested pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,24 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout />,
-  },
-  {
-    path: '/about',
-    element: <About />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <NewsLetter />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
   },
 ]);
 
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
     <div>
-      <h1>HomeLayout</h1>
-      <Link to="/about">about page</Link>
-      <a href="https://www.google.com">google</a>
+      <nav>navbar</nav>
+      <Outlet />
     </div>
   );
 };
