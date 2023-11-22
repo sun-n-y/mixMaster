743500f22313e0c058d7d8934ab73b2cdd7f196f
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-22 12:47:21 -0500

    add components import

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  NewsLetter,
+} from './pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h2>home page</h2>,
+    element: <HomeLayout />,
   },
   {
     path: '/about',
-    element: (
-      <div>
-        <h2>about page</h2>
-      </div>
-    ),
+    element: <About />,
   },
 ]);
 
