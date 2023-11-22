7fefdeb1bd4ddbbe626eba94f8812ec769c79ac2
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-22 15:26:11 -0500

    add link component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>About</h2>
+      <Link to="/">homepage</Link>
+    </div>
+  );
 };
 export default About;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <h2>HomeLayout</h2>;
+  return (
+    <div>
+      <h1>HomeLayout</h1>
+      <Link to="/about">about page</Link>
+      <a href="https://www.google.com">google</a>
+    </div>
+  );
 };
 export default HomeLayout;
