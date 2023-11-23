bc306091e708c30ead0eabcd40dc3b00b493df39
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-23 14:34:23 -0500

    add fetch drinks

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,18 @@
+import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
 export const loader = async () => {
-  return 'af';
+  const searchTerm = '';
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: response.data.drinks, searchTerm: searchTerm };
 };
 
 const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { searchTerm, drinks } = useLoaderData();
+  console.log(drinks);
   return <h2>Landing</h2>;
 };
 export default Landing;
