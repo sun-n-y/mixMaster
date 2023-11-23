e31c962508e184070342b53972c454325ed7d57d
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-23 10:53:16 -0500

    add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <span className="logo">MixMaster</span>
+        <div className="nav-links">
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+          <NavLink to="/newsletter" className="nav-link">
+            NewsLetter
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
