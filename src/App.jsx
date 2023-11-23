29e4bdd6bf7262ccd9b1051a7f41631ef5cedea7
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-23 14:14:40 -0500

    add loader for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   Landing,
   NewsLetter,
 } from './pages';
+import { loader as landingLoader } from './pages/Landing';
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        loader: landingLoader,
       },
       {
         path: 'cocktail',
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,12 @@
+import { useLoaderData } from 'react-router-dom';
+
+export const loader = async () => {
+  return 'af';
+};
+
 const Landing = () => {
+  const data = useLoaderData();
+  console.log(data);
   return <h2>Landing</h2>;
 };
 export default Landing;
