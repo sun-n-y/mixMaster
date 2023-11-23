c2e1b2972825b0ece3e938cd5ad921eb0f5dcc89
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-23 12:33:25 -0500

    add error handling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,28 @@
+import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/not-found.svg';
+
 const Error = () => {
-  return <h2>Error</h2>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="404" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
