772566a9635f16bcc59c51e34eed9b9bdb69823d
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-23 15:39:48 -0500

    add cocktaill cards

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         loader: landingLoader,
       },
       {
-        path: 'cocktail',
+        path: 'cocktail/:id',
         element: <Cocktail />,
       },
       {
