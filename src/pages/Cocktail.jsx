97dc22e92c16d6bc03e5e481d9fe8b9ee805b778
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-24 12:08:46 -0500

    add single cocktail render

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -14,7 +14,50 @@ export const loader = async ({ params }) => {
 
 const Cocktail = () => {
   const { id, data } = useLoaderData();
+  const singleDrink = data.drinks[0];
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
 
-  return <h2>Cocktail</h2>;
+  return (
+    <Wrapper>
+      <header>
+        <Link to="\" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Cocktail;
