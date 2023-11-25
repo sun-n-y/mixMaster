e9592a5539649e0f26c37e6aa1518912e919f697
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-25 16:46:07 -0500

    add search form

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
@@ -15,6 +16,7 @@ const Landing = () => {
   const { searchTerm, drinks } = useLoaderData();
   return (
     <>
+      <SearchForm />
       <CocktailList drinks={drinks} />
     </>
   );
