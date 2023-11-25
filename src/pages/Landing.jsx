3ba6f5b05572937d492e71345f42cceb3a6b66a8
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-25 18:14:52 -0500

    add checkpoint

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,19 +2,32 @@ import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 import CocktailList from '../components/CocktailList';
 import SearchForm from '../components/SearchForm';
+import { useQuery } from '@tanstack/react-query';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get('search') || '';
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: response.data.drinks, searchTerm: searchTerm };
+  const { data: drinks } = () => useQuery(searchCocktailsQuery(searchTerm));
+
+  return { searchTerm };
 };
 
 const Landing = () => {
   const { searchTerm, drinks } = useLoaderData();
+
   return (
     <>
       <SearchForm searchTerm={searchTerm} />
